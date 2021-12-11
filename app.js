d929f503473560675c9d61c73e71099e389f16fd
Darkestry <73218288+Darkestry@users.noreply.github.com>
2021-12-11 02:47:25 +0100

    Added features #4

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 let cookies = 0.0;  
-let money = 15000;
+let money = 15000000;
 let purity = 0.2;
 let sellers = 0;
 let interval = 100; //100ms
 let trailers = 0;
+let houses = 0;
 let grandmas = 0;
+let students = 0;
+let bakers = 0;
+let bakeries = 0;
+
+let baker1Amount = document.getElementById("baker1Amount");
+let baker2Amount = document.getElementById("baker2Amount");
+let baker3Amount = document.getElementById("baker3Amount");
 
 document.getElementById("buyBaker1").style.display = "none";
+document.getElementById("buyBaker2").style.display = "none";
+document.getElementById("buyBaker3").style.display = "none";
+
+baker1Amount.style.display = "none";
+baker2Amount.style.display = "none";
+baker3Amount.style.display = "none";
 
 function update() {
     document.getElementById("cookiecount").innerText = Math.round(cookies) + " Cookies";
-    document.getElementById("cookierate").innerText = Math.round(grandmas * 0.4) + " Cookies/sec";
+    document.getElementById("cookierate").innerText = Math.round((students * 0.4) + (grandmas * 4.4) + (bakers * 13.8)) + " Cookies/sec";
     document.getElementById("moneycount").innerText = Math.round(money) + " Dollars";
     document.getElementById("moneyrate").innerText = "$" + Math.round(20 * purity * sellers) + "/sec";
     document.getElementById("dealerDescription").innerText = sellers + " / 1000" + " Sellers"
@@ -30,6 +44,13 @@ function sell() {
     }
 }
 
+function playAudio(audio) {
+    let clickdown = new Audio(`http://orteil.dashnet.org/cookieclicker/snd/${audio}.mp3`);
+        clickdown.volume = 0.2;
+        clickdown.load();
+        clickdown.play();
+} 
+
 const baker1description = document.getElementById("baker1Description");
 const baker1image = document.getElementById("Baker1image");
 
@@ -41,18 +62,60 @@ function unlockBaker1() {
             baker1image.style.maxWidth = "100%";
             document.getElementById("Baker1").style.borderWidth = "0px";
             purity += 0.07;
-            document.getElementById("qualitycount").innerText = purity * 100 + "% Quality";
+            document.getElementById("qualitycount").innerText = Math.round(purity * 100) + "% Quality";
+            baker1Amount.style.display = "grid";
+            document.getElementById("buyBaker1").style.display = "grid";
         }
         trailers += 1;
-        document.getElementById("buyBaker1").style.display = "grid";
-        baker1description.innerText = grandmas + " / " + (trailers * 5) + " Grandmas";
-        let clickdown = new Audio('http://orteil.dashnet.org/cookieclicker/snd/buy4.mp3');
-        clickdown.volume = 0.2;
-        clickdown.load();
-        clickdown.play();
+        baker1Amount.innerText = trailers;
+        baker1description.innerText = students + " / " + (trailers * 5) + " Students";
+        playAudio("buy4")
     }
 }
 
+const baker2description = document.getElementById("baker2Description");
+const baker2image = document.getElementById("Baker2image");
+
+function unlockBaker2() {
+    if (money >= 100000) {
+        money -= 100000;
+        if(houses == 0){
+            baker2image.src="house.png";
+            baker2image.style.maxWidth = "100%";
+            document.getElementById("Baker2").style.borderWidth = "0px";
+            purity += 0.03;
+            document.getElementById("qualitycount").innerText = Math.round(purity * 100) + "% Quality";
+            baker2Amount.style.display = "grid";
+            document.getElementById("buyBaker2").style.display = "grid";
+        }
+        houses += 1;
+        baker2Amount.innerText = houses;
+        baker2description.innerText = grandmas + " / " + (houses * 10) + " Grandmas";
+        playAudio("buy4")
+    }
+}
+
+const baker3description = document.getElementById("baker3Description");
+const baker3image = document.getElementById("Baker3image");
+
+function unlockBaker3() {
+    if (money >= 1000000) {
+        money -= 1000000;
+        if(bakeries == 0){
+            baker3image.src="Bakery.png";
+            baker3image.style.maxWidth = "100%";
+            document.getElementById("Baker3").style.borderWidth = "0px";
+            purity += 0.06;
+            document.getElementById("qualitycount").innerText = Math.round(purity * 100) + "% Quality";
+            baker3Amount.style.display = "grid";
+            document.getElementById("buyBaker3").style.display = "grid";
+        }
+        bakeries += 1;
+        baker3Amount.innerText = bakeries;
+        baker3description.innerText = bakers + " / " + (bakeries * 30) + " Bakers";
+        playAudio("buy4")
+    }
+}
 function buyDealer() {
     if (money >= 1000) {
         money -= 1000;
@@ -63,21 +126,34 @@ function buyDealer() {
 }
 
 function buyBaker1() {
-    if (money >= 500 && grandmas < (trailers * 5)) {
+    if (money >= 500 && students < (trailers * 5)) {
         money -= 500;
-        grandmas += 1;
-        baker1description.innerText = grandmas + " / " + (trailers * 5) + " Grandmas";
+        students += 1;
+        baker1description.innerText = students + " / " + (trailers * 5) + " Students";
         playAudio("buy3")
         update();
     }
 }
 
-function playAudio(audio) {
-    let clickdown = new Audio(`http://orteil.dashnet.org/cookieclicker/snd/${audio}.mp3`);
-        clickdown.volume = 0.2;
-        clickdown.load();
-        clickdown.play();
-} 
+function buyBaker2() {
+    if (money >= 5000 && grandmas < (houses * 10)) {
+        money -= 5000;
+        grandmas += 1;
+        baker2description.innerText = grandmas + " / " + (houses * 10) + " Grandmas";
+        playAudio("buy3")
+        update();
+    }
+}
+
+function buyBaker3() {
+    if (money >= 50000 && bakers < (bakeries * 30)) {
+        money -= 50000;
+        bakers += 1;
+        baker3description.innerText = bakers + " / " + (bakeries * 30) + " Bakers";
+        playAudio("buy3")
+        update();
+    }
+}
 
 function round11(num){
     return Math.round(num*(Math.pow(10, 10))) / Math.pow(10, 10)
@@ -97,7 +173,7 @@ function autoSell() {
 }
 
 function autoCook() {
-    cookies += 0.4 * grandmas / (1000/interval);
+    cookies += (0.4 * students) + (4.4 * grandmas) + (13.8 * bakers) / (1000/interval);
     cookies = round11(cookies);
 }
 
